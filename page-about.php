15494ec61f4b6f66c3fe4ae1d08698496683c867
github user name <sensha5172@gmail.com>
2026-03-08 16:18:32 +0900

    update

--- page-about.php
+++ page-about.php
@@ -1,36 +1,37 @@
 <?php get_header(); ?>
-<?php if ( have_posts() ) : ?>
-  <?php while ( have_posts() ) : the_post(); ?>
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : the_post(); ?>
 
-  <main>
-    <section class="about_section">
-      <h2 class="sec_title">会社概要</h2>
-      <table class="company_info">
-        <tr>
-          <th>会社名</th>
-          <td>株式会社Test</td>
-        </tr>
-        <tr>
-          <th>設立</th>
-          <td>2000年4月</td>
-        </tr>
-        <tr>
-          <th>所在地</th>
-          <td>福岡市中央区舞鶴</td>
-        </tr>
-        <tr>
-          <th>代表者</th>
-          <td>田中太郎</td>
-        </tr>
-        <tr>
-          <th>事業内容</th>
-          <td>WEB制作</td>
-        </tr>
-      </table>
-    </section>
+    <main>
+      <section class="about_section">
+        <h2 class="sec_title">会社概要</h2>
+        <table class="company_info">
 
-  </main>
+          <tr>
+            <th>会社名</th>
+            <td>株式会社Test</td>
+          </tr>
+          <tr>
+            <th>設立</th>
+            <td>2000年4月</td>
+          </tr>
+          <tr>
+            <th>所在地</th>
+            <td>福岡市中央区舞鶴</td>
+          </tr>
+          <tr>
+            <th>代表者</th>
+            <td>田中太郎</td>
+          </tr>
+          <tr>
+            <th>事業内容</th>
+            <td>WEB制作</td>
+          </tr>
+        </table>
+      </section>
 
-  <?php endwhile;?>
-<?php endif;?>
-<?php get_footer();?>
+    </main>
+
+  <?php endwhile; ?>
+<?php endif; ?>
+<?php get_footer(); ?>
