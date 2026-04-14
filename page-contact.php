ec48695a44a6ec88089ea2a91ece4b665b954af4
takahashi kenzo <linkworks.sendai04@gmail.com>
2026-04-14 11:00:58 +0900

    update

--- page-contact.php
+++ page-contact.php
@@ -1,24 +1,25 @@
 <?php echo '【テンプレート】' . basename(__FILE__) . "<br>"; ?>
 
-<main>
-  <section>
-    <?php get_header(); ?>
-    <?php if (have_posts()) : ?>
-      <?php while (have_posts()) : the_post(); ?>
-
-        <main>
-          <h1 class="page_title">お問い合わせ</h1>
+<?php get_header(); ?>
 
 
-          <div class="form_wrap">
-            <!-- <form action=""> -->
-            <?php echo do_shortcode('[contact-form-7 id="86df0b3"]') ?>
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : the_post(); ?>
 
-          </div>
-        </main>
+    <main>
+      <h1 class="page_title">お問い合わせ</h1>
 
-      <?php endwhile; ?>
-    <?php endif; ?>
-  </section>
-</main>
+
+      <div class="form_wrap">
+        <!-- <form action=""> -->
+        <?php echo do_shortcode('[contact-form-7 id="86df0b3"]') ?>
+
+      </div>
+
+    </main>
+  <?php endwhile; ?>
+<?php endif; ?>
+
+
+
 <?php get_footer(); ?>
