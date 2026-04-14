75a6565fe93dda414e5670b7e1f82c33619426e4
takahashi kenzo <linkworks.sendai04@gmail.com>
2026-04-14 14:33:50 +0900

    update

--- archive-works.php
+++ archive-works.php
@@ -2,14 +2,16 @@
 
 
 <main>
-  <h1 class="page_title">制作実績</h1>
+
 
   <section class="news_section">
+
+    <h1 class="page_title">制作実績</h1>
 
     <div class="works_container">
 
       <a href="" class="works_item">
-        <img src="<?php echo get_template_directory_uri(); ?>/img/news.jpg" alt="" class="works_img">
+        <img src="<?php echo get_template_directory_uri("img/mainvisual.jpg"); ?>" alt="" class="works_img">
         <div class="works_info">
           <ul class="works_category">
             <li>WEB制作</li>
